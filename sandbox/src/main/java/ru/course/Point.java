a41c17d87150c016179f303a95fe2fcd594b921e
Денис Захаров <z.d.vlad96@mail.ru>
2023-06-05 21:36:51 +0400

    Создали метод в классе Point для определения расстояния между двумя точками

--- sandbox/src/main/java/ru/course/Point.java
+++ sandbox/src/main/java/ru/course/Point.java
@@ -1,7 +1,22 @@
 package ru.course;
 
 public class Point {
-    //х и у - координаты точек на плоскости
-    double x;
-    double y;
+    //х1 и у1 - координаты первой точки на плоскости
+    //х2 и у2 - координаты второй точки на плоскости
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+
+    public Point(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    //метод определения расстояния между двумя точками
+    public double distance() {
+        return Math.sqrt(Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2));
+    }
 }
