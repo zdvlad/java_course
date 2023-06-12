1bdb92f83cb595b7a328ef66f543b09c39ff8d22
Денис Захаров <z.d.vlad96@mail.ru>
2023-06-12 21:06:46 +0400

    -добавили ContactCreation тест -рефакторинг кода

---
+++ addressbook-web-tests/src/test/java/ru/course/addressbook/ContactCreationTest.java
@@ -0,0 +1,105 @@
+package ru.course.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ContactCreationTest {
+  private WebDriver wd;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    openMainPage();
+    login("admin", "secret");
+  }
+
+  private void openMainPage() {
+    wd.get("http://localhost/addressbook/");
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
+    initContactCreation();
+    fillContactsData(new ContactData("Denis", "Zakharov", "9023557076", "z.d.vlad96@mail.ru"));
+    submitContactCreation();
+    returnHomePage();
+  }
+
+  private void returnHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitContactCreation() {
+    wd.findElement(By.xpath("//input[21]")).click();
+  }
+
+  private void initContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  private void fillContactsData(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirtsName());
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getSecondName());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getPhoneNumber());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+  }
+
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.id("LoginForm")).submit();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = wd.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/course/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.course.addressbook;
+
+public class ContactData {
+    private final String firtsName;
+    private final String secondName;
+    private final String phoneNumber;
+    private final String email;
+
+    public ContactData(String firtsName, String secondName, String phoneNumber, String email) {
+        this.firtsName = firtsName;
+        this.secondName = secondName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirtsName() {
+        return firtsName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
