a3f90b8197e10b66b03f6bedd78baff6141c88b0
Денис Захаров <z.d.vlad96@mail.ru>
2023-07-12 21:50:49 +0400

    Поправил задание 11

---
+++ addressbook-web-tests/src/test/java/ru/course/addressbook/tests/ContactOptionsTest.java
@@ -0,0 +1,45 @@
+package ru.course.addressbook.tests;
+
+import org.hamcrest.MatcherAssert;
+import org.testng.annotations.Test;
+import ru.course.addressbook.model.ContactData;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactOptionsTest extends TestBase {
+
+    @Test
+    public void testAllOptions() throws Exception {
+        app.contact().returnToMainPage();
+        ContactData contactData = app.contact().all().iterator().next();
+
+        ContactData contactDataFromEditForm = app.contact().fromEditForm(contactData);
+
+        assertThat(contactData.getAllPhones(), equalTo(mergeAllPhones(contactDataFromEditForm)));
+        assertThat(contactData.getAddress(), equalTo(contactDataFromEditForm.getAddress().trim()));
+        assertThat(contactData.getAllEmails(), equalTo(mergeAllEmails(contactDataFromEditForm)));
+    }
+
+    private String mergeAllPhones(ContactData contact) {
+        return Arrays.asList(contact.getHomePhone().trim(), contact.getMobilePhone().trim(), contact.getWorkPhone().trim())
+                .stream().filter((s) -> ! s.equals(""))
+                .map(ContactOptionsTest::cleanedPhones)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private String mergeAllEmails(ContactData contact) {
+        return Arrays.asList(contact.getEmail().trim(), contact.getEmail2().trim(), contact.getEmail3().trim())
+                .stream().filter((s) -> ! s.equals(""))
+                .collect(Collectors.joining("\n"));
+    }
+
+
+    public static String cleanedPhones(String phones) {
+        return phones.replaceAll("\\s", "").replaceAll("[-()]","");
+    }
+
+}
