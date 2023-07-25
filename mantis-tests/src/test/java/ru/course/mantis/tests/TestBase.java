9b0d61e1f2d47435c216c6c77d95427a2302e7e6
Денис Захаров <z.d.vlad96@mail.ru>
2023-07-25 20:18:05 +0400

    Добавлен новый модуль Mantis

---
+++ mantis-tests/src/test/java/ru/course/mantis/tests/TestBase.java
@@ -0,0 +1,22 @@
+package ru.course.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.course.mantis.appmanager.ApplicationManager;
+
+
+public class TestBase {
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+}
